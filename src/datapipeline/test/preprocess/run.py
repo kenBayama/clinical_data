dc39a71f416903b08a0dd16a00fce74b4886d17d
kb <ken.bayama@gmail.com>
2020-10-01 19:08:00 +0200

    removing unecessary process and preprocess data from their repo

--- src/datapipeline/test/preprocess/run.py
+++ src/datapipeline/test/preprocess/run.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import os
 import re
-import logging
+from datapipeline.utils.loggers import Preprocessing_logger
 
 
 from datapipeline.utils.io  import (load_data, write_preprocessed_data, 
@@ -10,48 +10,66 @@ from datapipeline.utils.io  import (load_data, write_preprocessed_data,
 from datapipeline.utils.copy  import copy_csv
 
 
-logger = logging.getLogger(__name__)
-
-
-raw_repo = 'data/test/raw/'
-preprocess_repo = 'data/test/preprocess/'
-process_repo = 'data/test/process/'
-
-list_of_files = os.listdir(raw_repo)
-
-
 def preprocess_c_trials (clinicalT):
-	"""
-	This function allow you to store all the main information concerning the raw files
-	"""
-	clinicalT = clinicalT[clinicalT['scientific_title'].str.strip().map(len)>1].dropna()
-	clinicalT_date = pd.to_datetime(clinicalT['date'])
-	clinicalT['date'] =  clinicalT_date.dt.strftime('%d/%m/%Y')
-	clinicalT['journal'] = clinicalT['journal'].str.replace(r'(\\.*$)','') 
-	clinicalT['scientific_title'] = clinicalT['scientific_title'].str.replace(r'(\\.*$)','') 
-	return clinicalT
+    """
+    Description :
+        This function allow you to clean the raw data from clinical trials source*
+        by : 
+            Removing the space in the beguinning and the end of the scientific_title column
+            Dropping the row with NaN data in it
+            Converting the date column into a panda datetime column
+            Formatting the datetime column to have the same format for each column
+            Cleaning the journal column to remove hexadecimal characters
+            Cleaning the  journal column to remove hexadecimal characters
+
+        
+    Args:
+        pubmed: panda dataframe object
+    
+    Returns:
+        output: panda.dataframe
+    """
+
+    clinicalT = clinicalT[clinicalT['scientific_title'].str.strip().map(len)>1].dropna()
+    clinicalT_date = pd.to_datetime(clinicalT['date'])
+    clinicalT['date'] =  clinicalT_date.dt.strftime('%d/%m/%Y')
+    clinicalT['journal'] = clinicalT['journal'].str.replace(r'(\\.*$)','') 
+    clinicalT['scientific_title'] = clinicalT['scientific_title'].str.replace(r'(\\.*$)','') 
+    return clinicalT
 
 def preprocess_pubmed(pubmed):
-	"""
-	This function allow you to store all the main information concerning the raw files
-	"""
-	pubmed = pubmed.drop('id',1)
-	pubmed ['id'] = pubmed.reset_index().index
-	pubmed['date'] = pd.to_datetime(pubmed['date']).dt.strftime('%d/%m/%Y')
-	pubmed['title'] = pubmed['title'].str.replace('"','')
-	return pubmed
+    """
+    Description :
+        This function allow you to clean the raw data from pubmed source by :
+            Removing the wrong id and setting a proper one
+            Formatting the datetime column to have the same format for each column
+            Cleaning the title column by removing unnecessary double quote chracter
+        
+    Args:
+        pubmed: panda dataframe object
+    
+    Returns:
+        output: panda.dataframe
+
+    """
+    pubmed = pubmed.drop('id',1)
+    pubmed ['id'] = pubmed.reset_index().index
+    pubmed['date'] = pd.to_datetime(pubmed['date']).dt.strftime('%d/%m/%Y')
+    pubmed['title'] = pubmed['title'].str.replace('"','')
+    return pubmed
 
 
 
-def main():
+def main(raw_repo,preprocess_repo):
 	
+	list_of_files = os.listdir(raw_repo)
 	list_of_file_object = list(create_file_object(list_of_files,raw_repo))
 	dict_ds = load_data (list_of_file_object)
 
-	"""
-	This function do the preprocessing nécessary in order to clean
-	the dataset.
-	"""
+	
+	#The main function do the preprocessing necessary in order to clean
+	#the datasets from pubmed, clinical_trials and drug raw files.
+	
 
 	for category in dict_ds :
 		if  category == 'clinicaltrials':
@@ -61,7 +79,7 @@ def main():
 				write_preprocessed_data(preproc_c_trials,dict_ds[category]
 					.name,preprocess_repo,"csv")
 			except:
-				logger.error(f"The preprocessing of clinicaltrials data raise an error")
+				Preprocessing_logger.error("The preprocessing of clinicaltrials data raise an error")
 				raise
 		elif category == 'pubmed':
 			try:	
@@ -69,7 +87,7 @@ def main():
 				write_preprocessed_data(preproc_pubmed,dict_ds[category]
 					.name,preprocess_repo,"csv")
 			except:
-				logger.error(f"The preprocessing of pubmed data raised an error")
+				Preprocessing_logger.error("The preprocessing of pubmed data raised an error")
 				raise
 				
 	
@@ -78,6 +96,6 @@ def main():
 				copy_csv(raw_repo + dict_ds[category].name,preprocess_repo 
 					+ dict_ds[category].name)
 			except:
-				logger.error(f"The copy of the raw data with no preprocessing went wrong")
+				Preprocessing_logger.error("The copy of the raw data with no preprocessing went wrong")
 				raise
 				
--- src/datapipeline/test/process/run.py
+++ src/datapipeline/test/process/run.py
@@ -6,124 +6,168 @@ import os
 from datapipeline.utils.io  import (load_data, write_preprocessed_data, 
 									create_file_object, file_object)
 
-raw_repo = 'data/test/raw/'
-preprocess_repo = 'data/test/preprocess/'
-process_repo = 'data/test/process/'
 
-list_of_files = os.listdir(preprocess_repo)
-
-
-def who_is_there(string, list_of_drug):
+def find_word_in_text(string, list_of_drug):
+    """
+    Description :
+        This function allow you to find if a list of word is in a string :
+             Turning all letter to uppercase
+             Checking the presence of a word in a string
+             Removing space
+             Appending the new element in a list 
+             Transforming a list of string to a string with comma for separation
+    Args:
+        string: string 
+        list_of_drug : dict, key : name of the drug
+                             value :  atccode of the drug
+    
+    Returns:
+        output: string
+    """
     drug_present = []
     for word in list_of_drug:
         if word.upper() in string.upper():
             drug_present.append(list_of_drug[word].strip())
+
     return  ','.join(map(str, drug_present))
 
 def clean_column(string):
-    str2 = list(set(string.split(",")))
-    return list(filter(None,str2))
+    """
+    Description :
+        This function allow you to transform a string separated by comas in a list of string :
 
-def prepare_c_trials (c_trials,list_of_drug):
-    c_trials['atccode'] = c_trials['scientific_title'].str.upper()\
-    	.apply(lambda string : who_is_there(string,list_of_drug))
-    c_trials = c_trials.apply(lambda x: x.astype(str).str.upper())
-    return c_trials
+            Transforming the string in a list
+            Removing the duplicated 
+            Removing empty space
+    Args:
+        string: string 
 
-def prepare_pubmed (pubmed,list_of_drug):
-    pubmed['atccode'] = pubmed['title'].str.upper()\
-    	.apply(lambda string : who_is_there(string,list_of_drug))
-    pubmed = pubmed.apply(lambda x: x.astype(str).str.upper())
-    return pubmed
+    Returns:
+        output: list of string
+    """
+    list_of_strng = list(set(string.split(",")))
+    list_of_strng = list(filter(None,list_of_strng))
+
+    return list_of_strng
+
+
+def process_data (dataset,list_of_drug,column_name):
+    """
+    Description :
+        This function allow you to transform the clinicals_trials or pubmed data 
+        in order to make the foreign keys to the drug data:
+
+            Finding the drugs name in each scientic title or title column
+            Turning every letter in uppercase for later use
+            Renaming the date column
+    Args:
+        c_trials: pandas.dataframe 
+        list_of_drug : dict, key : name of the drug
+                             value :  atccode of the drug
+        column_name : string
+    
+    Returns:
+        output: pandas.dataframe
+    """
+    dataset['atccode'] = dataset[column_name].str.upper()\
+        .apply(lambda string : find_word_in_text(string,list_of_drug))
+    dataset = dataset.apply(lambda x: x.astype(str).str.upper())
+    dataset = dataset.rename(columns = {"date":"date_mention"})
+
+    return dataset
 
 def create_journal_ds(c_trials,pubmed):
-    journal = pd.concat([pubmed[['journal','date','atccode']],
-    	c_trials[['journal','date','atccode']]])
+    """
+    Description :
+        This function allow you to create the journal dataset from the clinicals trials data 
+        and the pubmed data :
+
+            keeping only the journal, date_mention and atccode column
+            Joining the two dataframe.
+            Turning every letter to uppercase
+            Grouping by the column journal
+            Aggregating by concatenation of string 
+            Cleaning by using the clean_column function
+            Creating an Index
+
+    Args:
+        c_trials: pandas.dataframe 
+        pubmed : pandas.dataframe 
+    
+    Returns:
+        output: pandas.dataframe
+    """
+    journal = pd.concat([pubmed[['journal','date_mention','atccode']],
+    	c_trials[['journal','date_mention','atccode']]])
     journal = journal.apply(lambda x: x.astype(str).str.upper())
     journal = journal.groupby('journal')\
     	.agg(lambda column: ",".join(column)).reset_index('journal')
-    journal = journal.applymap(clean_column)
+    journal[['date_mention','atccode']] = journal[['date_mention','atccode']].applymap(clean_column)
     journal ['id']= journal.reset_index().index.astype(str)
 
     return journal
 
-def prepare_for_drug(dataset,name):
-    dataset['atccode'] = dataset['atccode']\
-    	.apply(lambda s: s.split(",")).to_frame()
-
-    dataset = dataset.explode('atccode')
-    dataset = dataset.groupby('atccode')\
-    	.agg(lambda x : ",".join(x)).reset_index('atccode')
-
-    dataset = dataset.rename(columns = {"id":name +"_id"})
-    return dataset
 
 def create_dict_of_drug(drug):
-	list_of_drug = (drug['drug'] + "," + drug['atccode']).tolist()
-	lst = [s.split(",") for s in list_of_drug]
-	lst = [{s[0]: s[1]} for s in lst]
-	list_of_drug = dict((key,d[key]) for d in lst for key in d)
-	return list_of_drug
+    """
+    Description :
+        This function allow you to create a dictionnary linking the name of a drug
+        and its atccode: 
 
+            Creating a list of string from concatenation of two columns drug and atccode
+            Transforming the list of string in list of list of string
+            Converting that list of list in a list of dict
+            Converting the list of dict in a dict
+
+    Args:
+        drug: pandas.dataframe 
     
+    Returns:
+        output: dict 
+    """
+    list_of_drug = (drug['drug'] + "," + drug['atccode']).tolist()
+    lst = [s.split(",") for s in list_of_drug]
+    lst = [{s[0]: s[1]} for s in lst]
+    list_of_drug = dict((key,d[key]) for d in lst for key in d)
 
-def main():
+    return list_of_drug
 
-	list_of_file_object = list(create_file_object(list_of_files,preprocess_repo))
-	dict_ds = load_data (list_of_file_object)
+def main(preprocess_repo,process_repo):
 
-	drug = dict_ds["drugs"].dataset
-	list_of_drug = create_dict_of_drug(drug)
 
-	"""
-	prepare the clinical_trials and pubmed data to create the foreign keys 
-	to drugs and for the implementation of the journal table data 
+    list_of_files = os.listdir(preprocess_repo)
+    list_of_file_object = list(create_file_object(list_of_files,preprocess_repo))
+    dict_ds = load_data (list_of_file_object)
 
-	"""
-	try:
-		prepared_c_trials = prepare_c_trials(dict_ds['clinicaltrials']\
-			.dataset,list_of_drug)
+    drug = dict_ds["drugs"].dataset
+    list_of_drug = create_dict_of_drug(drug)
 
-		preprared_pubmed = prepare_pubmed(dict_ds['pubmed'].dataset,list_of_drug)
-		
-		processed_journal = create_journal_ds(prepared_c_trials,preprared_pubmed)
-	except:
-		print("An error was raised while preparing the journal data")
-
-	"""
-	prepare the journal, clinical ans pubmed data
-	 to create the drugs table data with all the right foreign keys
 	
-	"""
-	try:
-
-		pubmed_f_drugs = prepare_for_drug(preprared_pubmed,"pubmed")
-		c_trials_f_drugs = prepare_for_drug(prepared_c_trials,"clinical_trials")
-
-
-		journal_f_drugs = processed_journal.explode('atccode')
-		journal_f_drugs = journal_f_drugs.groupby('atccode')\
-			.agg(lambda x: ",".join(x)).reset_index('atccode')
-
-		journal_f_drugs = journal_f_drugs.rename(columns= {"id":"journal_id"})
-	except:
-		print("An error was raised while preparing the journal data")
-
-	try:
-		drugs = drug\
-			.merge(c_trials_f_drugs[['atccode','clinical_trials_id']],
-				on='atccode', how='left')\
-			.merge(pubmed_f_drugs[['atccode','pubmed_id']], on= 'atccode', how='left')\
-			.merge(journal_f_drugs[['atccode','journal_id']], on= 'atccode', how='left')\
-			.fillna("")
-	except: 
-		print(" An error was raised while preparing the drugs table")
+	#prepare the clinical_trials and pubmed data to create the foreign keys 
+	#to drugs and for the implementation of the journal table data 
 
 	
 
-	write_preprocessed_data(processed_journal,"journal",process_repo,"json")
-	write_preprocessed_data(drugs,"drug",process_repo,"json")
-	write_preprocessed_data(prepared_c_trials,"clinical_trials",process_repo,"json")
-	write_preprocessed_data(preprared_pubmed,"pubmed",process_repo,"json")
+    try:
+        prepared_c_trials = process_data(dict_ds['clinicaltrials']\
+            .dataset,list_of_drug,"scientific_title")
 
+        preprared_pubmed = process_data(dict_ds['pubmed'].dataset,list_of_drug,
+            "title")
+
+        processed_journal = create_journal_ds(prepared_c_trials,preprared_pubmed)
+
+    except:
+        Processing_logger.error("An error was raised while preparing the journal data")
+
+    try:
+
+        write_preprocessed_data(processed_journal,"journal",process_repo,"json")
+        write_preprocessed_data(drug,"drug",process_repo,"json")
+        write_preprocessed_data(prepared_c_trials,"clinical_trials",process_repo,"json")
+        write_preprocessed_data(preprared_pubmed,"pubmed",process_repo,"json")
+
+    except:
+        Processing_logger.error("An error was raised while writting the processed data")
+      
 
